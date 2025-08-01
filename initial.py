2ef77a2a0cf3c6be905e5bd2bca8383e91ac32d1
Sakuntha Hansaka <104773152+SakunthaMV@users.noreply.github.com>
2025-08-02 00:18:35 +0530

    Update initial.py
    Test 2

--- initial.py
+++ initial.py
@@ -1,1 +1,16 @@
-print("Successfully created action.")
+def access_bybit_profile():
+    return HTTP(
+        testnet=False,
+        api_key="jbZNqIEuoC64EbGHZR",
+        api_secret="gJqIqk6F6DNGqokiDZcED5dAILO7aqIuC1ur",
+    )
+
+def get_balance():
+    try:
+        session = access_bybit_profile()
+        resp = session.get_wallet_balance(accountType="UNIFIED",coin="USDT")["result"]["list"][0]["coin"][0]["walletBalance"]
+        return resp
+    except Exception as err:
+        print(err)
+
+print(get_balance())
